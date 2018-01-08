2ff2eb3a77e55f44bf7155e94abca0e0cab17bf4
Graeme O <graeme.ong>
2018-01-08 13:54:55 +0800

    [GraemeO] Use setup configuration file to define module definition instead of within the code

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(name='package_me_up',
-      version='0.0.1',
-      description='Learning on how to package up Python module',
-      author='GraemeO',
-      author_email='iaintgivingnothing',
-      url='https://graemeo.github.io',
-      install_requires=[
-          'requests'
-      ])
+setup()
