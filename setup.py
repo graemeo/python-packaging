4d7045c4ab2eebfc761666cfa2bb283e8f36a0e6
Graeme O <graeme.ong>
2018-01-08 11:02:34 +0800

    [GraemeO] Add gitignore and update setup.py file to satisfy the some requirements

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='package_me_up',
       version='0.0.1',
       description='Learning on how to package up Python module',
       author='GraemeO',
+      author_email='iaintgivingnothing',
       url='https://graemeo.github.io')
